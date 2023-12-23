dabb6b40051523988ada8f1c7986de46e69b6374
Rema <bouvysull1v4n@gmail.com>
2023-12-23 17:13:09 +0100

    update CombatEtape

--- CombatEtape.py
+++ CombatEtape.py
@@ -2,5 +2,8 @@ from enum import Enum
 
 class CombatEtape(Enum):
     COMBAT_FIGHT = 0
-    JOUEUR_ATTAQUE = 1
-    BOT_ATTAQUE = 2
+    COMBAT_ITEM = 1
+    COMBAT_PKMN = 2
+    COMBAT_RUN = 3
+    JOUEUR_ATTAQUE = 4
+    BOT_ATTAQUE = 5
